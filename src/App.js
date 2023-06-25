dc91ba93855b85d597c2c007a1afe04f1978ac38
AppolinFotso <appolin.s.fotso@gmail.com>
2023-06-25 14:47:58 +0100

    Added map methods to retrieve each country info

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
 import EachCountryInfo from "./EachCountryInfo";
+import data from "../rest-countries-api-with-color-theme-switcher-master/data.json";
+import { useState } from "react";
+import { nanoid } from "nanoid";
 
 function App() {
+  const [countryInfo, setCountryInfo] = useState(data);
+  const countrydata = countryInfo.map((country) => {
+    return (
+      <EachCountryInfo
+        key={nanoid()}
+        flags={country.flags}
+        name={country.name}
+        population={country.population}
+        region={country.region}
+        capital={country.capital}
+      />
+    );
+  });
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Country Flags</p>
       </header>
+      <div className="countries-container">{countrydata}</div>
     </div>
   );
 }
--- src/EachCountryInfo.js
+++ src/EachCountryInfo.js
@@ -1,5 +1,15 @@
 import React from "react";
 
-function EachCountryInfo() {}
+function EachCountryInfo(props) {
+  return (
+    <div>
+      <img src={props.flags.png} />
+      <p>{props.name}</p>
+      <p>Population: {props.population}</p>
+      <p>Region: {props.region}</p>
+      <p>Capital: {props.capital}</p>
+    </div>
+  );
+}
 
 export default EachCountryInfo;
